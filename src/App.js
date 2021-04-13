7a4933cbb737386201f5dd754ec01a17d3d4d2d0
Taiyahm <taiyahm23@gmail.com>
2021-04-13 07:30:02 -0400

    header setup

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "react-particles-js";
+/*import "react-typist/dist/Typist.css"*/
+import Navbar from  "./components/Navbar";
+import Header from  "./components/Header";
+
+function App() {
+  return (
+    <>   
+    <Particles
+    className = "particles-canvas"
+      params={{
+        particles: {
+          number: {
+            value: 60,
+            density:{
+              enable: true,
+              value_area:900,
+            }
+          },
+          shape: {
+            type:"circle",
+            stroke: {
+              width: 6,
+              color: "#FF7F50",
+          } }
+        }
+      }}
+    />
+    <Navbar />
+    <Header />
+    </>
+  );
+}
+
+export default App;
